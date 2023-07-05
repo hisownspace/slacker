eb7295065fcde00ff5f9602f99a8afbdf8f1bd96
David Nash <hisownspace@gmail.com>
2023-07-05 18:24:42 -0400

    configure frontend to emit socket to backend and display all new chats

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -35,7 +35,7 @@ function App() {
           <Route path="/signup">
             <SignupFormPage />
           </Route>
-          <Route path="/channel/channelId">
+          <Route path="/channel/:channelId">
             <Channel />
           </Route>
         </Switch>
--- react-app/src/components/Channel/Channel.js
+++ react-app/src/components/Channel/Channel.js
@@ -1,7 +1,50 @@
+import { io } from "socket.io-client";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+let socket;
+
 function Channel() {
+  const [messages, setMessages] = useState([]);
+  const [chatInput, setChatInput] = useState("");
+  const user = useSelector((state) => state.session.user);
+
+  useEffect(() => {
+    socket = io();
+
+    socket.on("chat", (chat) => {
+      console.log(chat);
+      // window.alert(chat);
+      setMessages((messages) => [...messages, chat]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const updateChatInput = (e) => {
+    setChatInput(e.target.value);
+  };
+
+  const sendChat = (e) => {
+    e.preventDefault();
+    socket.emit("chat", { user: user.username, msg: chatInput });
+
+    setChatInput("");
+  };
   return (
     <>
       <h1>Channel</h1>
+      <div>
+        {messages.map((message, idx) => (
+          <div key={idx}>{`${message.user}: ${message.msg}`}</div>
+        ))}
+      </div>
+      <form onSubmit={sendChat}>
+        <input value={chatInput} onChange={updateChatInput} />
+        <button type="submit">Send</button>
+      </form>
     </>
   );
 }
